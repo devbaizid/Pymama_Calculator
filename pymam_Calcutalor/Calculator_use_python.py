f72b3251fc89dabfd8a8956a1b5ccba589b13f7b
Tanvir WEB <91944929+TanvirIsalmBaizid@users.noreply.github.com>
2021-10-27 19:49:25 +0600

    Pymama_Calculator is Cool
    Pymama_Calculator
    # nice 
    #Good 
    #cool

---
+++ pymam_Calcutalor/Calculator_use_python.py
@@ -0,0 +1,35 @@
+
+# use Python just python
+
+print("Welcome to my calcutor-1 ")
+
+i = 1
+while i <= 3:
+    print(i *"////////Thanks...")
+    i += 1
+# main Function
+def calcutor(name):
+
+
+ return your_type * name
+
+# comment
+your_type=input("Your Type: ")
+first_numbrs = int(input("Enter Your First Number:"))
+second_number= int(input("Enter You second Number:"))
+
+
+if your_type =="+":
+    print(first_numbrs+second_number)
+elif your_type == "-":
+    print(first_numbrs-second_number)
+elif your_type == "*":
+    print(first_numbrs*second_number)
+elif your_type == '/':
+    print(first_numbrs/second_number)
+else:
+    print("Not Found in there ....... Thanks for Try")
+
+
+if __name__ == "__main__":
+    calcutor(404)
